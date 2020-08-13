2a743c7834bad5c54ca3b0fbdd4c5fdb973e1083
Lucas Marques <lucas.msilva09@gmail.com>
2020-08-13 17:56:23 -0300

    Creating tests for auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,10 +30,6 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an authProvider');
-  }
-
   return context;
 }
 
